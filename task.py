e0de3c4f1fda39feeea6ad87e8872d94ed2792e3
Brouney <piotreg98@gmail.com>
2020-03-19 17:11:34 +0100

    check_uploar

--- task.py
+++ task.py
@@ -25,4 +25,45 @@
 #
 #Delete these comments before commit!
 #Good luck.
-    
+
+import random
+import time
+class Car:
+    def __init__(self):
+        super().__init__()
+        self.angle = 0
+        self.speed = 0
+    def act(self,event,angl):
+        event.run(angl)
+
+class turn_left:
+    def run(self,angl):
+        angl.angle -= random.randint(0,20)
+        #print("turn left, current angle: "+str(angl.angle))
+
+class turn_right:
+    def run(self,angl):
+        angl.angle += random.randint(0,20)
+        #print("turn right,current angle: "+str(angl.angle))
+
+class up_speed:
+      def run(self,angl):
+            angl.speed += random.randint(0,20)    
+            #print("speed up: current speed: "+str(angl.speed))
+class down_speed:
+      def run(self,angl):
+            angl.speed -= random.randint(0,20)
+            #print("speed down: current speed: "+str(angl.speed))
+
+
+left = turn_left()
+right = turn_right()
+up = up_speed()
+down = down_speed()
+car1 = Car()
+temp = [left,right,up,down]
+while True:
+    active = random.randint(0,3)
+    car1.act(temp[active],car1)
+    print("speed: "+str(car1.speed)+" angle: "+str(car1.angle))
+    time.sleep(1)
